d7f3dce811253f605aac6d5c11f7e6c05004d3e8
Abheek Dhawan <abheekd@protonmail.com>
2021-07-26 16:34:24 -0500

    Add initial content
    - Add quick description of the bot
    - Set up directory to serve static content (particularly images)
    - Add brand/icon to navbar
    - Implement AOS for cool animations on scroll

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/static', express.static(__dirname + "/static"));
 
 const port = process.env.PORT || 8000;
 
@@ -12,6 +13,10 @@ app.set('view engine', 'pug');
 app.get("/orbital", async (req, res) => {
   return res.render('index');
 });
+
+app.get("/static", async (req, res) => {
+  return res.send(__dirname + "/static");
+})
 
 app.get("/favicon.ico", async (req, res) => {
   return res.sendFile(__dirname + "/static/favicon.ico");
