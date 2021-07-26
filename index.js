e37cea64ed05ea8c721f84d9105b75fb256f3c29
Abheek Dhawan <abheekd@protonmail.com>
2021-07-26 15:17:17 -0500

    Add initial site using Bootstrap and Pug.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const app = require("express")();
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 8000;
+
+app.set('view engine', 'pug');
 
 app.get("/orbital", async (req, res) => {
-  return res.send("<h1>HELLO!</h1>");
+  return res.render('index');
 });
+
+app.get("/favicon.ico", async (req, res) => {
+  return res.sendFile(__dirname + "/static/favicon.ico");
+})
 
 app.listen(8000, () => {
   console.log("Running on port 8000!");
